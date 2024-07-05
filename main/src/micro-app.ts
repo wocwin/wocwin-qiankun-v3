18bc07259472934469230e22767e68ad126c4bf8
wocwin <wocwin@163.com>
2024-07-05 12:38:57 +0800

    init

---
+++ main/src/micro-app.ts
@@ -0,0 +1,25 @@
+const microApps = [
+  {
+    name: "wocwin-vue2",
+    entry: import.meta.env.VITE_APP_SUB_VUE2,
+    activeRule: "/wocwin-vue2/"
+  },
+  {
+    name: "wocwin-admin",
+    entry: import.meta.env.VITE_APP_SUB_VUE3,
+    // entry: '//localhost:3300/wocwin-admin/',
+    activeRule: "/wocwin-admin/"
+  }
+];
+
+const apps = microApps.map(item => {
+  return {
+    ...item,
+    container: "#app", // 子应用挂载的div
+    props: {
+      routerBase: item.activeRule // 下发基础路由
+    }
+  };
+});
+
+export default apps;
